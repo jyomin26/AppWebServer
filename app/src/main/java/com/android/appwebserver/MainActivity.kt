b0856da2404bf59fd35deb70d2bcb3b7e04b2311
jyoti.minhas <jyoti.minhas@trantorinc.com>
2020-11-27 11:08:57 +0530

    Added Sample code for Application Web Server

---
+++ app/src/main/java/com/android/appwebserver/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.android.appwebserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import com.rx2androidnetworking.Rx2AndroidNetworking
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+private val TAG = "app-ui"
+var deviceBaseUrl: String? = "http://127.0.0.1:5001" //local host IP Address
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Start web server
+        WebServer.startServer()
+        setClickListener()
+    }
+
+    /**
+     * Register click listeners
+     */
+    private fun setClickListener() {
+        btnGetApi.setOnClickListener {
+            callGetApi()
+        }
+        btnPostApi.setOnClickListener {
+            callPostApi()
+        }
+        btnPutApi.setOnClickListener {
+            callPutApi()
+        }
+        btnDeleteApi.setOnClickListener {
+            callDeleteApi()
+        }
+    }
+
+    /**
+     * Call to Webserve get Api
+     */
+    fun callGetApi() {
+        val url = deviceBaseUrl + "/status"  //create url
+        var job: Disposable? = null
+        job = Rx2AndroidNetworking.get(url)
+            .build()
+            .getObjectObservable(JSONObject::class.java)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ status ->
+                tvGetApiResponse.text = status.getString("status")
+                job?.dispose()
+            }, { erorr ->
+                Log.w(TAG, "GET $url failed: $erorr")
+                job?.dispose()
+            })
+    }
+
+    /**
+     * Call to webserver post Api
+     */
+    fun callPostApi() {
+        val data = "android"
+        val url = deviceBaseUrl + "/username/$data"
+        var job: Disposable? = null
+        job = Rx2AndroidNetworking.post(url)
+            .build()
+            .getObjectObservable(JSONObject::class.java)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ status ->
+                tvPostApiResponse.text = status.getString("response")
+                job?.dispose()
+            }, { erorr ->
+                Log.w(TAG, "Post $url failed: $erorr")
+                job?.dispose()
+            })
+    }
+
+    /**
+     * Call to webserver put Api
+     */
+    fun callPutApi() {
+        val url = deviceBaseUrl + "/update"
+        Rx2AndroidNetworking.put(url).addJSONObjectBody(JSONObject().put("key", "value"))
+            .build().getAsString(object : StringRequestListener {
+                override fun onResponse(response: String?) {
+                    tvPutApiResponse.text = response
+                    Log.d(TAG, "Put api response $response")
+                }
+
+                override fun onError(anError: ANError?) {
+                    Log.w(TAG, "Failed to get output detection: ${anError}")
+                }
+
+            })
+    }
+
+    /**
+     * Call to webserver delete Api
+     */
+    fun callDeleteApi() {
+        val url = deviceBaseUrl + "/delete"
+        var job: Disposable? = null
+        job = Rx2AndroidNetworking.post(url)
+            .build()
+            .getObjectObservable(JSONObject::class.java)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ status ->
+                tvPostApiResponse.text = status.getString("response")
+                job?.dispose()
+            }, { erorr ->
+                Log.w(TAG, "Post $url failed: $erorr")
+                job?.dispose()
+            })
+    }
+
+}
---
+++ app/src/main/java/com/android/appwebserver/WebServer.kt
@@ -0,0 +1,87 @@
+package com.android.appwebserver
+
+import android.util.Log
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CachingHeaders
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.http.CacheControl
+import io.ktor.request.receiveText
+import io.ktor.response.cacheControl
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.put
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import org.json.JSONObject
+import java.util.concurrent.TimeUnit
+
+private val TAG = "app-server"
+private const val second = 1000L
+
+/**
+ * Server class to handle Apis Calls
+ */
+object WebServer {
+    private var server = embeddedServer(Netty, port = 5001) {
+        Log.d(TAG, "Initializing the system server...")
+
+        install(CallLogging)
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
+        install(CachingHeaders)
+
+        routing {
+            get("/status") {
+                // Stopped saving cache on client end
+                call.response.cacheControl(CacheControl.NoCache(null))
+                //
+                call.respond(JSONObject().put("status", "Server is running"))
+            }
+
+            post("/username/{data?}") {
+                // Stopped saving cache on client end
+                call.response.cacheControl(CacheControl.NoCache(null))
+                // fetch post data
+                val postData = call.parameters["data"]
+                if (postData.isNullOrEmpty()) {
+                    Log.w(TAG, "Missing data")
+                    call.respond(JSONObject().put("response", "Missing data"))
+                    return@post
+                }
+                call.respond(JSONObject().put("response", "$postData successfully"))
+            }
+
+            put("/update/{data?}") {
+                // Stopped saving cache on client end
+                call.response.cacheControl(CacheControl.NoCache(null))
+                val putData = call.receiveText()
+                if (putData.isNullOrEmpty()) {
+                    Log.w(TAG, "Missing data")
+                    call.respond(JSONObject().put("response", "Missing data"))
+                    return@put
+                }
+                call.respond(JSONObject().put("response", "$putData successfully"))
+            }
+        }
+    }
+
+    /**
+     * Start server
+     */
+    fun startServer() {
+        server.start(wait = false)
+        Log.i(TAG, "System server has initialized")
+    }
+
+    fun close() {
+        server.stop(second, second, TimeUnit.MILLISECONDS)
+    }
+}
